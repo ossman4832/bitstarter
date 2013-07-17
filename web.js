ac8f37ecdc1a160019ff79c4c2bf43f1fdd480b4
ossman4832 <aosseyrane@gmail.com>
2013-07-17 17:43:16 +0000

    Read data from file

--- web.js
+++ web.js
@@ -6,11 +6,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-//var msgFile = fs.readFileSync('index.html');
-
 
 app.get('/', function(request, response) {
-//   fs.readFileSync('index.html'); 
    var msgFile = fs.readFileSync('index.html');
    var msgFile2 = msgFile.toString();
    response.send(msgFile2);
