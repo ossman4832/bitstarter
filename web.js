464174e618c59567f8a32e278e550a8b58669dc9
ossman4832 <aosseyrane@gmail.com>
2013-07-24 03:14:20 +0000

    Assignment 4 Question 1 files

--- web.js
+++ web.js
@@ -6,14 +6,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var htmlfile = 'index.html';
 
 app.get('/', function(request, response) {
-   var msgFile = fs.readFileSync('index.html');
-   var msgFile2 = msgFile.toString();
-   response.send(msgFile2);
+   var html = fs.readFileSync(htmlfile).toString();
+   response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
